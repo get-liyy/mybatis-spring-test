931cf1421aa2d15cc4d0cba86293620fb153ec51
liyanyong <729679898@qq.com>
2018-03-30 16:19:07 +0800

    Zero:日志记录

--- src/main/java/com/liyy/mybatis/controller/task/SpringTaskTest.java
+++ src/main/java/com/liyy/mybatis/controller/task/SpringTaskTest.java
@@ -22,12 +22,12 @@ public class SpringTaskTest {
 	 * fixedDelay:固定延迟(周期是以上一个调用任务的完成时间为基准，在上一个任务完成之后，5s后再次执行)
 	 * @author liyanyong
 	 */
-	@Scheduled(fixedRate = 5000)
+	@Scheduled(fixedRate = 50000)
 	public void test() {
 		String str = "定时任务执行咯咯咯咯咯咯:"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS").format(new Date());
 //		System.out.println(str);
-//		logger.info("INFO---"+str);
-//		logger.error("ERROR---"+str);
-//		logger.debug("DEBUG---"+str);
+		logger.info("INFO---"+str);
+		logger.error("ERROR---"+str);
+		logger.debug("DEBUG---"+str);
 	}
 }
